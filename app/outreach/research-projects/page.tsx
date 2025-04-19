37f3f6cea46df0b932000db4e394427719fa80d1
Sudipta Sardar <sudiptasbd@bicpu.edu.in>
2025-04-19 20:39:10 +0900

    removed broadcamb padding

--- app/outreach/research-projects/page.tsx
+++ app/outreach/research-projects/page.tsx
@@ -41,7 +41,7 @@ export default function ResearchProjectsPage() {
 
         <main className="container mx-auto px-4 py-20">
           <div className="max-w-4xl mx-auto">
-            <div className="mb-8 py-6">
+            <div className="mb-8">
               <BreadcrumbNavigation items={breadcrumbItems} />
 {/*               <Link
                 href="/#scientific-outreach"
