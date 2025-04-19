a2e5c661f0c3c495f32e79ceefc5ab3c91d170cd
Sudipta Sardar <sudiptasbd@bicpu.edu.in>
2025-04-19 18:51:47 +0900

    gallery

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -37,7 +37,7 @@ export default async function GalleryPage() {
           </div>
 
           <h1 className="text-3xl md:text-4xl font-bold mb-12">Gallery</h1>
-          <p>{filenames}</p>
+          <p>{filenames[1]}</p>
 
           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
             {Array.from({ length: 16 }).map((_, index) => (
